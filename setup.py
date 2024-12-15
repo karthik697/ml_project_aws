027f4d46611cf52f1577852a5bf2e2fa2ac65759
karth-29 <karthikeyan.bose@tigeranalytics.com>
2024-12-15 13:53:15 +0530

    adding all the files to git

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    requirement =[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+        
+        if HYPEN_E_DOT  in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup(
+    name ='ml_project_aws',
+    version='0.0.1',
+    author ='karthikeyan',
+    author_email='subashkarthik002@gmail.com',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+)
